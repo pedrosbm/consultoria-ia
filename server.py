7e031a7e58c7e3888d1eec0cd6ee53386b96427c
Pedro Sena <pedrosbmartinazzo@hotmail.com>
2024-05-11 17:55:22 -0300

    servidor flask

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+import pickle
+
+app = Flask(__name__)
+
+with open("./CreditAi.pkl", "rb") as arquivo:
+    modelo = pickle.load(arquivo)
+
+
+@app.route("/prever", methods=["POST"])
+def prever():
+    dados = request.json
+
+    previsao = modelo.predict([dados["features"]])
+
+    return jsonify({"previsao": previsao.tolist()})
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
