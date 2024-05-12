6d5236dda8a3275b8041df9db67826ff3618d262
Pedro Sena <pedrosbmartinazzo@hotmail.com>
2024-05-11 21:45:05 -0300

    fim do flask

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pickle
 
 app = Flask(__name__)
+CORS(app, origins='*')
 
 with open("./CreditAi.pkl", "rb") as arquivo:
     modelo = pickle.load(arquivo)
